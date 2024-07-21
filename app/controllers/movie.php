1052a58cd6602d5c3ddb4254a378701b99fd1109
em-au <146881865+em-au@users.noreply.github.com>
2024-07-21 23:12:44 +0000

    Reorganize code for rating function

--- app/controllers/movie.php
+++ app/controllers/movie.php
@@ -14,7 +14,7 @@ class Movie extends Controller {
     
     $api = $this->model('Api');
     $title = $_REQUEST['movie'];
-    // Replace spaces in movie title with hyphen
+    // Replace spaces in movie title with hyphen to concatenate with OMDb API query URL
     $title = str_replace(" ", "-", $title);
     $movie = $api->search_movie($title);
 
@@ -48,50 +48,47 @@ class Movie extends Controller {
       }
 
       $user_rating = $this->model('Rating');
-
+      $movie_decoded = urldecode($movie); // Handle any spaces in movie title
+      
       // If user has already rated this movie, delete the old rating first
-      if ($user_rating->getUserRating($movie, $_SESSION['user_id'])) {
-        $user_rating->delete_rating($_SESSION['user_id'], $movie);
+      $row = $user_rating->getUserRating($_SESSION['user_id'], $movie_decoded);
+      if (!empty($row)) {
+        $user_rating->delete_rating($_SESSION['user_id'], $movie_decoded);
+      }  
+
+      // Check that the rating is an integer and between 1-5 (inclusive)
+      if (!is_int($rating) || $rating < 1 || $rating > 5) {
+        header('location: /movie');
+        die;
       }
-      // urldecode() to handle any spaces in movie title
-      $title = urldecode($movie);
-      $user_rating->add_rating($_SESSION['user_id'], $title, $rating);
-      // want this to redirect back to search result page and not /movie/rating/title/4
-      //header('location: /movie/result'); // goes back to /movie with search bar, not result
+
+      // Add rating
+      $user_rating->add_rating($_SESSION['user_id'], $movie_decoded, $rating);
+
+      // Send user back to search results with updated information (ie their new rating)
       $api = $this->model('Api');
-      // Replace spaces in movie title with hyphen
-      //$title = str_replace(" ", "-", $title);
-      $movie = $api->search_movie($title);
-      //echo "<pre>";
-      //print_r($movie); die; // doesnt' work if spaces are %20
+      $title = str_replace(" ", "-", $movie_decoded);
+      $movie = $api->search_movie($title); // Use the movie title to grab the same movie from OMDb
       
-        // If movie is found, get ratings of the movie from the model --> MOVE TO SEPARATE FXN?
-        if ($movie['Response'] != "False") {
-          //$ratings = $this->model('Rating');
-          $rows = $user_rating->getRatings($title);
-          // Format the date
-          foreach ($rows as &$row) {
-            $timestamp = strtotime($row['date_added']);
-            $row['date_added'] = date("F j, Y", $timestamp);
-          }
-        } 
-      
-        //If movie is found, generate reviews of the movie --> MOVE TO SEPARATE FXN?
-        $api = $this->model('Api');
-        $movie_for_review = $movie['Title']; // can change to $title
-        $movie_opinions = array("amazing", "good", "average", "bad", "awful", 
-        "boring", "interesting");
+      // A movie was found
+      if ($movie['Response'] != "False") {
+        // Get ratings from all users for this movie
+        $all_ratings = $this->get_ratings($movie['Title']);
 
-        // Get random number of reviews
-        $num_reviews = rand(2,4);
-        for ($i = 0; $i < $num_reviews; $i++) {
-          $response = $api->get_review($movie_for_review, $movie_opinions[rand(0,6)]);
-          // Grab only the text part of the response
-          $review_text = $response['candidates'][0]['content']['parts'][0]['text'];
-          $reviews[$i] = $review_text;
-        }
+        // Get the current user's rating of the movie 
+        $user_rating = $this->get_user_rating($movie['Title']);
 
-      $this->view('movie/result', ['movie' => $movie, 'ratings' => $rows, 'reviews' => $reviews]);
+        // Get AI-generated reviews of the movie
+        $reviews = $this->get_reviews($movie['Title']);
+      }
+      else if ($movie['Response'] == "False") {
+        // SHOW ERROR MESSAGE NICELY
+      }
+
+      $this->view('movie/result', ['movie' => $movie, 
+                  'ratings' => $all_ratings, 
+                  'user_rating' => $user_rating,
+                  'reviews' => $reviews]);
     }
 
     public function get_user_rating($movie) {
